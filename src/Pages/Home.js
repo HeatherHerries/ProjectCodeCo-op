b32f5034f7349e097c05f12edda888696fcea70c
HeatherH <hherries@yahoo.com>
2023-02-15 07:02:49 -0700

    Started Featured Projects section.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../Styles/Home.css'
 import MainHeroImg from '../Assets/Images/SVGImages/HomeHeroDT.svg'
+import FPImg from '../Assets/Images/SVGImages/GirlWorkingatComputer.svg'
 import HomeNav from '../Components/HomeNav'
 import JoinBtnRed from '../Components/JoinBtnRed'
 import SocialLinks from '../Components/SocialLinks'
@@ -27,6 +28,15 @@ function Home() {
 
         <div className="social-links">
           <SocialLinks />
+        </div>
+      </div>
+
+      <div className="home-featured-projects">
+        <h1 className="featured-projects-title">Featured Projects</h1>
+        <p className="featured-projects-text">&#123;projectCode&#125;Co-op team members are working tirelessly on projects to show the world, but don’t want anyone to see what we are capable of until it absolutely perfect! Check back shortly and be ready for your mind to be blown! </p>
+        <img src={FPImg} alt="" className="featured-projects-img" />
+        <div className="join-btn-red">
+          <JoinBtnRed />
         </div>
       </div>
     </div>
