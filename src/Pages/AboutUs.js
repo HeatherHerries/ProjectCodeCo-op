c9a1c0b4ef407685ee0fc2b6deca6a9c9994a127
HeatherH <hherries@yahoo.com>
2023-02-20 14:25:23 -0700

    Completed About Us hero section.

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import "../Styles/AboutUs.css"
+import AboutUsHeroImg from "../Assets/Images/AboutUsHeroImage.png"
 
 function AboutUs() {
   return (
     <div>
-      <h1>About Us</h1>
+      <div className="about-us-hero">
+        <img src={AboutUsHeroImg} alt="The two founders of ProjectCode Co-op" />
+        <h1 className="about-us-hero-title">Two Coders, One Desire</h1>
+      </div>
     </div>
   )
 }
