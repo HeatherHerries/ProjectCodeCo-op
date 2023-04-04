052fb52e42a252d6ef4f278cb6bd3a2a3c047121
HeatherH <hherries@yahoo.com>
2023-04-04 14:17:55 -0600

    Completed blue button.

--- src/Components/JoinBtnBlue.js
+++ src/Components/JoinBtnBlue.js
@@ -4,7 +4,11 @@ import '../Styles/JoinBtnBlue.css'
 function JoinBtnBlue() {
   return (
     <>
-      <button className='btn-blue'>Join &#123;projectCode&#125; Co-op</button>
+      <button className='join-btn-blue'>
+        <a className="button-link"href="https://www.meetup.com/">
+        Join &#123;projectCode&#125; Co-op
+        </a>
+        </button>
     </>
   )
 }
