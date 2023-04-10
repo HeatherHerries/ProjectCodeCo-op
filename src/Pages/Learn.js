1c5dccb84307e374a41a0f1f4fedd0b3ee336cb8
HeatherH <hherries@yahoo.com>
2023-04-10 16:15:12 -0600

    Started setting up the Learn Page.

--- src/Pages/Learn.js
+++ src/Pages/Learn.js
@@ -3,53 +3,36 @@ import "../Styles/Learn.css";
 import JoinBtnRed from "../Components/JoinBtnRed";
 import NavBar from "../Components/NavBar";
 import Footer from "../Components/Footer";
+import EMC2 from "../Assets/Images/SVGImages/E=MC2Graffiti.svg"
 import Einstein from "../Assets/Images/Einstein.png";
 import Design from "../Assets/Images/SVGImages/Explosion.svg";
 
-// useState for both development and design Icons then use their pagelinks as the setIcon (future state).
 
-//  use props for blue square Icons62
-
-export default function Learn(props) {
+export default function Learn() {
   return (
-    <>
-      <div className="learn-container">
+    <div className="learn-container">
+      <div className="nav-bar">
         <NavBar />
-        <div className="learn-hero">
-          <div>
-            <h1> ProjectCodeCo - op Gives Good Brain </h1>
-
-            <h6>
-              “Self-education is, I firmly believe, the only kind of education
-              there is.” – Issac Asimov
-            </h6>
-          </div>
-          <div>
-            <img src={Einstein} alt="pic of einstein" />
-            <div className="learn-hero-btn">
-              <JoinBtnRed />
-            </div>
-          </div>
-        </div>{" "}
       </div>
 
-      <div className="learn-section2">
-        {" "}
-        <div>
-          <img src={Design} alt="Design Icon" />
-          <h1>Design</h1>
-        </div>
-        <div>
-          <img
-            src={Design}
-            alt="
-          Design
-          Icon"
-          />
-          <h2>Development</h2>
+      <div className="learn-hero">
+        <div className="learn-title">
+          <h1>&#123;projectCode&#125;Co-op Gives Good Brain </h1>
+
+          <img src={EMC2} alt="E=MC2 Graffiti" />
+
+          <img src={Einstein} alt="" />
+
+          <p className="learn-quote">“Self-education is, I firmly believe, the only kind of education there is.” – Issac Asimov</p>
+
+          <div className="learn-button">
+            <JoinBtnRed />
+          </div>
         </div>
       </div>
-      <Footer />
-    </>
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
   );
 }
